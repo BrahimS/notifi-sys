8d8753390ce81a792ee4480ce66714d808c5827c
brahims <bsourny@gmail.com>
2025-04-01 19:50:18 +0200

    feat(app): implement application root

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import NotificationsWidget from "./components/notification-widget/NotificationsWidget.vue";
+import Article from "./components/ui/article/Article.vue";
+import Header from "./components/ui/header/Header.vue";
+import Section from "./components/ui/section/Section.vue";
+import Main from "./components/ui/main/Main.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		NotificationsWidget,
+		Header,
+		Article,
+		Section,
+		Main,
+	},
+});
+</script>
+<template>
+	<Main>
+		<div class="max-w-7xl mx-auto">
+			<Header>
+				<h1 class="text-3xl font-semibold">Notification management system</h1>
+			</Header>
+			<Section>
+				<Article>
+					<notifications-widget namespace="Instance One" />
+				</Article>
+
+				<Article>
+					<notifications-widget namespace="Instance Two" />
+				</Article>
+			</Section>
+		</div>
+	</Main>
+</template>
